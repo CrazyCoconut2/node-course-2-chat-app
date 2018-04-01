da8b71022cbdb3e8ba9b2d699f87d49a615b987f
Mohammed Tigrini <mtigrini@gmail.com>
2018-04-01 11:23:50 -0400

    Added connect and disconnect methods.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,34 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
+
+// heroku app :  https://pumpkin-custard-64046.herokuapp.com/
+// heroku git : https://git.heroku.com/pumpkin-custard-64046.git
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
 var app = express();    // THIS IS THE PROPERS WAY TO USE EXPRESS
                         // WE DO NOT USE IT DIRECTLY ON EXPRESS
+var server = http.createServer(app);
+// Next thing we do is to also configure the server to use
+// socket.io
+var io=socketIO(server); // we pass in parameters the server we wanna use
+
+//Emitting and communicating events
+
+// built in events
+io.on('connection', (socket) => {
+    console.log('New user connected.');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected.');
+    })
+});  
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Server is up on port " + port);
 })
 
